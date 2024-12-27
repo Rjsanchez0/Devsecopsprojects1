78610c42aed4880ecf67527f1e775c57edee295c
Raul <rjsanchez062006@gmail.com>
2024-12-27 09:33:35 -0600

    commit

--- main.tf
+++ main.tf
@@ -2,23 +2,28 @@ provider "aws" {
   region = "us-west-2" # Oregon region
 }
 
-resource "aws_s3_bucket" "projectdevops" {
-  bucket = "projectdevops"
+resource "aws_s3_bucket" "projectdevops1" {
+  bucket = "projectdevops1"
 }
+
 
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
-    bucket = "projectdevops"
-    region = "us-west-2"
+    bucket = "projectdevops1"              # Replace with your actual S3 bucket name
+    key    = "terraform/state.tfstate" # Path to the state file in the S3 bucket
+    region = "us-west-2"           # Replace with the desired AWS region
   }
 }
 
 resource "aws_instance" "example_instance" {
-  ami           = "ami-07d9cf938edb0739b" # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"              # Change to the desired instance type
+  ami           = "ami-07d9cf938edb0739b" # Replace with a valid Amazon Linux 2 AMI for eu-west-2
+  instance_type = "t2.micro"              # Update to your desired instance type
 
   tags = {
-    Name = "example-instance"             # Update with your desired tag
+    Name = "example-instance"             # Replace with your desired tag name
   }
 }
+
+
+
+
